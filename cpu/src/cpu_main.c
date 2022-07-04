e9e4925338352241afc1e8e09d8ce1ca051f2732
lucas-marmo <lmarmo@frba.utn.edu.ar>
2022-07-04 11:51:17 -0300

    Se comenzó a integrar memoria a cpu

--- cpu/src/cpu_main.c
+++ cpu/src/cpu_main.c
@@ -10,6 +10,11 @@ char* reemplazo_tlb;
 bool hay_interrupcion;
 double alfa;
 uint64_t rafaga_real;
+uint16_t tam_pagina;
+uint16_t cant_ent_paginas;
+t_list* tlb;
+uint16_t pid_anterior=0;
+
 int main(void) {
 
 	inicializar_cpu();
@@ -20,7 +25,7 @@ int main(void) {
 		recv(cliente_socket_dispatch, &cop, sizeof(op_code), 0);
 		recv_proceso(cliente_socket_dispatch,pcb);
 		log_info(logger,"Proceso %d -> program counter %d - est %f", pcb->pid, pcb->pc, pcb->est_rafaga);
-
+		if(pcb->pid != pid_anterior ) limpiar_tlb();
 		struct timespec start;
 		clock_gettime(CLOCK_REALTIME,&start);
 		op_code estado = iniciar_ciclo_instruccion(pcb);
@@ -28,7 +33,7 @@ int main(void) {
 		clock_gettime(CLOCK_REALTIME, &end);
 		rafaga_real = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000;
 		calculo_estimacion(pcb, estado);
-
+		pid_anterior = pcb->pid;
 		log_info(logger,"Program counter %d (despues de ejecutar)",pcb->pc);
 		log_info(logger,"==============================================================");
 		send_proceso(cliente_socket_dispatch,pcb,estado);
@@ -58,6 +63,17 @@ void inicializar_cpu(){
 	entradas_tlb = config_get_int_value(config, "ENTRADAS_TLB");
 	reemplazo_tlb = config_get_string_value(config, "REEMPLAZO_TLB");
 	hay_interrupcion = false;
+	tlb=list_create();
+
+	inicializar_tlb();
+
+	/*for(int i=0;i< entradas_tlb;i++){
+			TLB_t *tlb_aux2 = malloc(sizeof(TLB_t));
+			tlb_aux2 = list_get(tlb,i);
+			log_info(logger,"Numero pagina: %d", tlb_aux2->pagina);
+			log_info(logger,"Numero pagina: %d", tlb_aux2->marco);
+			free(tlb_aux2);
+		}*/
 
 	operando_copy = 0;
 
@@ -69,6 +85,7 @@ void inicializar_cpu(){
 	if (conexion_memoria == 0){
 		log_error(logger,"Error al intentar conectarse a memoria :-(");
 		exit(EXIT_FAILURE);
+		recv_datos_necesarios(conexion_memoria,&tam_pagina, &cant_ent_paginas);
 	}
 
 	char* ip = config_get_string_value(config, "IP");
@@ -79,12 +96,41 @@ void inicializar_cpu(){
 
 	cliente_socket_dispatch = esperar_cliente(logger, "CPU_DISPATCH",server_cpu_dispatch);
 	cliente_socket_interrupt = esperar_cliente(logger, "CPU_INTERRUPT", server_cpu_interrupt);
-	recv(cliente_socket_dispatch,&alfa, sizeof(double), 0);
+	recv(cliente_socket_dispatch, &alfa, sizeof(double), 0);
 	log_info(logger,"Valor alfa %.2f",alfa);
 	pthread_t hilo_interrupciones;
 	pthread_create(&hilo_interrupciones,NULL,(void*)interrupcion,NULL);
 	pthread_detach(hilo_interrupciones);
 
+}
+
+
+void inicializar_tlb(){
+
+	for(int i=0;i< entradas_tlb;i++){
+		TLB_t *tlb_aux = crear_entrada_tlb(-1,-1);
+		list_add(tlb, tlb_aux);
+
+		//TLB_t *tlb_aux2 = list_get(tlb,i);
+		//log_info(logger,"Numero pagina: %d", tlb_aux2->pagina);
+		//log_info(logger,"Numero pagina: %d", tlb_aux2->marco);
+	}
+
+}
+
+
+void limpiar_tlb(){
+	list_clean(tlb);
+	inicializar_tlb(tlb);
+}
+
+TLB_t *crear_entrada_tlb(int32_t pagina, int32_t marco){
+	TLB_t *tlb_entrada= malloc(sizeof(TLB_t));
+
+	tlb_entrada->marco = marco;
+	tlb_entrada->pagina = pagina;
+	tlb_entrada->ultima_referencia = clock();
+	return tlb_entrada;
 }
 
 void apagar_cpu(){
@@ -101,9 +147,10 @@ op_code iniciar_ciclo_instruccion(PCB_t* pcb){
 
 		if(decode(instruccion_ejecutar)){
 			log_info(logger,"Aca busco el operando");
-		//Aca iria fetch operands y la ejecucion de copy
+			instruccion_ejecutar->arg2  = fetch_operands(instruccion_ejecutar->arg2,pcb->tabla_paginas);
+
 		}
-		estado = execute(instruccion_ejecutar);
+		estado = execute(instruccion_ejecutar,pcb->tabla_paginas);
 		if(estado == CONTINUE){
 			estado = check_interrupt();
 		}
@@ -124,27 +171,128 @@ int check_interrupt(){
 	return CONTINUE;
 }
 
+int fetch_operands(int direccion, uint32_t tabla){
+	return ejecutarRead(direccion,tabla);
+}
+
+
 int decode(instruccion_t* instruccion_ejecutar ){
 	return instruccion_ejecutar->operacion == 'C';
 }
 
-void ejecutarIO(){
+
+
+int ejecutarRead(uint32_t dir_logica,uint32_t tabla_paginas){
+	uint32_t dir_fisica;
+	int valor=8;
+	dir_fisica = traducir_direccion(dir_logica,tabla_paginas);
+	//Sería send_read(int fd, uint32_t nro_marco, uint16_t desplazamiento)
+	//send_instruccion(dir_fisica);
+	// Este de aca abajo te lo hacemos nosotros, tranqui
+	// valor = recv_valor_leido();
+	return valor;
+}
+
+
+
+void ejecutarWrite(uint32_t dir_logica,uint32_t valor,uint32_t tabla_paginas ){
+	uint32_t dir_fisica;
+	dir_fisica = traducir_direccion(dir_logica, tabla_paginas);
+	//Sería send_write(int fd, uint32_t nro_marco, uint16_t desplazamiento, uint32_t dato)
+	//send_instruccion(dir_fisica,valor)
+	// Este de aca abajo te lo hacemos nosotros, tranqui
+	//recv_verificacion
+}
+
+uint32_t traducir_direccion(uint32_t dir_logica,uint32_t tabla_paginas){
+
+	int32_t marco;
+	uint32_t numero_pagina = floor((double)dir_logica/(double)tam_pagina);
+	//Aca hay que fijarse si esta en la tlb
+	marco=presente_en_tlb(numero_pagina);
+
+	if(marco==-1){
+		uint32_t entrada_tabla_1 = floor((double)numero_pagina/(double)cant_ent_paginas);
+		uint32_t entrada_tabla_2 = numero_pagina % cant_ent_paginas;
+		// Seria send_solicitud_nro_tabla_2do_nivel(int fd, uint16_t pid, uint32_t nro_tabla_1er_nivel, uint32_t entrada_tabla)
+		//send_tabla(tabla_paginas,entrada_tabla_1);
+		// Este de aca abajo te lo hacemos nosotros, tranqui
+		//recv_tabla(&tabla_paginas2);
+		// Seria send_solicitud_nro_marco(int fd, uint16_t pid, uint32_t nro_tabla_2do_nivel, uint32_t entrada_tabla, uint32_t index_tabla_1er_nivel)
+		//send_tabla(tabla_paginas2,entrada_tabla_2);
+		// Este de aca abajo te lo hacemos nosotros, tranqui
+		//recv_tabla(&marco);
+		marco = rand()%11;
+		if(!marco_en_tlb(marco,numero_pagina)){
+			if(strcmp(reemplazo_tlb,"LRU"))reemplazo_tlb_LRU( numero_pagina,  marco );
+			else if(strcmp(reemplazo_tlb,"FIFO")) reemplazo_tlb_FIFO(numero_pagina,marco);
+		}
+	}
+
+	uint32_t desplazamiento = dir_logica - numero_pagina * tam_pagina;
+
+	return marco + desplazamiento;
+
 
 }
 
-void ejecutarRead(){
+
+
+bool pagina_marco_tlb(TLB_t* entrada,int32_t marco,int32_t pagina){//Para encontrar la lista a actualizar
+	return (entrada->marco== marco && entrada->pagina == pagina);
+}
+
+int32_t presente_en_tlb(uint32_t numero_pagina){
+
+	for(int i=0;i< entradas_tlb;i++){
+		TLB_t *tlb_aux = tlb_aux=list_get(tlb,i);
+		log_info(logger,"Numero pagina: %d",tlb_aux->pagina);
+		log_info(logger,"Numero marco: %d", tlb_aux->marco);
+		log_info(logger,"Ciclo cpu: %d",(int)tlb_aux->ultima_referencia);
+		if(tlb_aux->pagina==numero_pagina){
+			tlb_aux->ultima_referencia = clock();
+			return tlb_aux->marco;
+		}
+
+
+	}
+
+	return -1;
+}
+
+void reemplazo_tlb_FIFO(uint32_t numero_pagina, int32_t marco ){
+	list_remove_and_destroy_element(tlb,0,free);
+	TLB_t *tlb_entrada = crear_entrada_tlb(numero_pagina,marco);
+	list_add_in_index(tlb,entradas_tlb-1,tlb_entrada);
 
 }
 
-void ejecutarCopy(){
-
+bool menor(TLB_t* a,TLB_t* b){
+	return a->ultima_referencia<b->ultima_referencia;
 }
 
-void ejecutarWrite(){
 
+void reemplazo_tlb_LRU(uint32_t numero_pagina, int32_t marco ){
+
+	list_sort(tlb,*menor); //Preguntar como se supone que funciona esto
+	list_remove_and_destroy_element(tlb,0,free);
+	TLB_t *tlb_entrada = crear_entrada_tlb(numero_pagina,marco);
+	list_add_in_index(tlb,entradas_tlb-1,tlb_entrada);
 }
 
-int execute(instruccion_t* instruccion_ejecutar){
+bool marco_en_tlb(int32_t marco,int32_t pagina){//Para encontrar la lista a actualizar
+	for (int i=0;i< entradas_tlb;i++){
+		TLB_t *tlb_aux = list_get(tlb,i);
+		if(tlb_aux->marco==marco){
+			tlb_aux->pagina = pagina;
+			tlb_aux->ultima_referencia = clock();
+			return true;
+			}
+	}
+	return false;
+}
+
+int execute(instruccion_t* instruccion_ejecutar,uint32_t tabla_paginas){
 
 	switch (instruccion_ejecutar->operacion) {
 		case 'N':
@@ -157,15 +305,16 @@ int execute(instruccion_t* instruccion_ejecutar){
 			break;
 		case 'R':
 			log_info(logger,"Ejecutando READ");
-			ejecutarRead();
+
+			log_info(logger,"Valor leido %d",ejecutarRead(instruccion_ejecutar->arg1,tabla_paginas));
 			break;
-		case 'C':
+		case 'C': //Hizo read en fetch operands
 			log_info(logger,"Ejecutando COPY");
-			ejecutarCopy();
+			ejecutarWrite(instruccion_ejecutar->arg1,instruccion_ejecutar->arg2,tabla_paginas);
 			break;
 		case 'W':
 			log_info(logger,"Ejecutando WRITE");
-			ejecutarWrite();
+			ejecutarWrite(instruccion_ejecutar->arg1,instruccion_ejecutar->arg2,tabla_paginas);
 			break;
 		case 'E':
 			log_info(logger,"Ejecutando EXIT");
